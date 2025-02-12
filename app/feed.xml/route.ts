76cd1feab132282ade02ca0effcc7bc7679bae77
choporo <chopporo@gmail.com>
2025-02-12 16:22:41 +0900

    ing_fifth

--- app/feed.xml/route.ts
+++ app/feed.xml/route.ts
@@ -4,10 +4,12 @@ import RSS from "rss";
 export async function GET() {   
     const allPosts = await fetchPropertiesRss();
     const feed = new RSS({
-       title: '온천 갈 지도 | 국내 온천 도감',           description: "우라나라 사람들도 잘 모르는 국내 온천 소개",
+       title: '온천 갈 지도 | 국내 온천 도감',          
+       description: "우라나라 사람들도 잘 모르는 국내 온천 소개",
        generator: 'RSS for Node and Next.js',
-       feed_url: 'https://www.thermalbath.co.kr/feed.xml',
-       site_url: 'https://www.thermalbath.co.kr',copyright: `Copyright ${new Date().getFullYear().toString()}`,
+       feed_url: 'https://thermalbath.co.kr/feed.xml',
+       site_url: 'https://thermalbath.co.kr/sitemap.xml',
+       copyright: `Copyright ${new Date().getFullYear().toString()}`,
        language: 'ko-Kr', 
        pubDate: new Date().toUTCString(),
        ttl: 60,       
