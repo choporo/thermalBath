b11c00e9d7339593bce8951aeb072ce8e024137f
choporo <chopporo@gmail.com>
2025-02-27 23:41:20 +0900

    modal

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -76,25 +76,25 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   });
 
   return (
-    // <Modal>
-    <div className="lg:px-80 overflow-y-scroll">
-      <BreadCrumbs category={category} name={name} />
-      <ImageContainer image={property.image} name={property.name} />
-      <header className="flex justify-between gap-x-4 mt-2 items-center">
-        <h1 className="text-xl tracking-wider">{name}</h1>
-        <div className="flex gap-x-1 items-center mr-1">
-          <FaRegPenToSquare className="sm:w-4 sm:h-4 w-3 h-3 text-orange-500" />
-          <p className="text-xs mt-0.5 tracikng-wider">{rating}건↑</p>
-        </div>
-      </header>
-      <section className=" text-justify">
-        <PropertyDetails details={details} />
-        <Separator className="mt-2 mb-3 " />
-        <ThermalDetails property={thermal} />
-      </section>
-      <GoogleMap lat={latitude} lon={longitude} name={name} />
-    </div>
-    // </Modal>
+    <Modal>
+      <div className="p-2 overflow-y-scroll">
+        <BreadCrumbs category={category} name={name} />
+        <ImageContainer image={property.image} name={property.name} />
+        <header className="flex justify-between gap-x-4 mt-2 items-center">
+          <h1 className="text-xl tracking-wider">{name}</h1>
+          <div className="flex gap-x-1 items-center mr-1">
+            <FaRegPenToSquare className="sm:w-4 sm:h-4 w-3 h-3 text-orange-500" />
+            <p className="text-xs mt-0.5 tracikng-wider">{rating}건↑</p>
+          </div>
+        </header>
+        <section className=" text-justify">
+          <PropertyDetails details={details} />
+          <Separator className="mt-2 mb-3 " />
+          <ThermalDetails property={thermal} />
+        </section>
+        <GoogleMap lat={latitude} lon={longitude} name={name} />
+      </div>
+    </Modal>
   );
 }
 
