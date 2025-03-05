e6fca8f7b221b36e59c462646ef6e7f6b8e98cc6
choporo <chopporo@gmail.com>
2025-03-05 12:43:54 +0900

    hourly

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,19 @@
 import { fetchPropertiesSitemap } from "@/utils/action";
-import type { MetadataRoute } from "next";
+import type { MetadataRoute } from "next"
 
-export default async function Sitemap(): Promise<MetadataRoute.Sitemap>{
+export default async function sitemap(): Promise<MetadataRoute.Sitemap>{
     const properties = await fetchPropertiesSitemap()
     const posts = properties.map((post) =>({
         url: `https://thermalbath.co.kr/properties/${post.id}`,
-        lastModified: post.updateAt
-    }))
+        lastModified: post.updateAt,
+        priority: 0.8,
+        changeFrequency: 'hourly' as any 
+    })) 
     
     const routes = ["", "/about"].map((route) =>({
         url: `https://thermalbath.co.kr${route}`,
-        lastModified: new Date().toISOString().split("T")[0]
+        lastModified: new Date().toISOString().split("T")[0],
+        changeFrequency: 'hourly' as any 
     }))
     
     return[...routes, ...posts]
--- metadata-interface.d.ts
+++ metadata-interface.d.ts
@@ -2,12 +2,13 @@ type Sitemap = Array<{
     url: string
     lastModified?: string | Date
     changeFrequency?:
-        | "always"
-        | "hourly"
-        | "daily"
-        | "weekly"
-        | "yearly"
-        | "never"
+    | 'always'
+    | 'hourly'
+    | 'daily'
+    | 'weekly'
+    | 'monthly'
+    | 'yearly'
+    | 'never'
     priority?: number
 }>
 
