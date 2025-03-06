8af2372531bf35af3f1d2781ee9f4c5a8899bd44
choporo <chopporo@gmail.com>
2025-03-06 12:25:45 +0900

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import PcVersion from "@/components/about/PcVersion";
 import MobileVersion from "@/components/about/MobileVersion";
 import { Metadata } from "next";
+import { sendGTMEvent } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "한국의 온천",
@@ -19,7 +20,10 @@ export const metadata: Metadata = {
 function AboutPage() {
   return (
     <article className="sm:p-1">
-      <div className="flex justify-center items-center gap-x-2 sm:p-2">
+      <div
+        className="flex justify-center items-center gap-x-2 sm:p-2"
+        onClick={() => sendGTMEvent({ event: "clicked", value: "about" })}
+      >
         <h1 className="lg:text-3xl text-xl font-bold text-green-700 tracking-wider text-center leading-loose">
           한국의 <span className="text-red-600">온천</span>
         </h1>
